06d29e0e58991acc385a00a9380c3faaff81d463
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-18 22:57:17 -0500

    Colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/stories/**/*.{js,ts,jsx,tsx}',
   ],
+
   theme: {
+    colors: {
+      'white': '#f1f1f1',
+      'black': '#13131d',
+    },
+    plugins: [],
   },
-  plugins: [],
-}
+};
