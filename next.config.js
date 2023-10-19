aaebe65639bf79e37cfcc6109f8aa161878dcbf7
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-18 23:07:07 -0500

    Config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    i18n:{
+        defaultLocale: "es",
+        locales: ["es", "en"]
+    }
+}
 
 module.exports = nextConfig
