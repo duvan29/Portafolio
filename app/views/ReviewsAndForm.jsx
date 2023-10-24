f51e485e34d93b811af9aedd1a0d89a6ea613861
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-24 11:40:09 -0500

    Cambios

--- app/views/ReviewsAndForm.jsx
+++ app/views/ReviewsAndForm.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Reviews from '../components/Reviews';
 
 export default function ReviewsAndForm() {
 
     return (
         <div className="w-full pt-5 flex justify-between items-stretch">
           <div className="w-[50%] bg-gris-50 p-4 rounded-lg">
-            {/* <TimeLine/> */}
+            <Reviews/>
           </div>
           <div className="w-[50%] pl-4 rounded-lg flex flex-col">
             {/* <Proyects/> */}
