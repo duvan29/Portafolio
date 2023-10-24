cfad9cb75661f006d4fa7dc1429b52c5b2bce89e
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-24 14:02:58 -0500

    Cambios

--- app/components/timeLine.jsx
+++ app/components/timeLine.jsx
@@ -1,9 +1,11 @@
+import { useTranslations } from 'next-intl';
 import React from 'react';
 
 export default function TimeLine() {
+  const t = useTranslations("TimeLine");
   return (
-    <div className="w-full pt-5 flex justify-between items-center">
-        <h1>Time Line</h1>
+    <div className="w-full">
+        <h1 className="text-2xl font-semibold text-center mb-5">{t("title")}</h1>
     </div>
   );
 }
