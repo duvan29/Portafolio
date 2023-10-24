ae9c16fa29e378b637e4dd424f63992cf1564c76
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-24 12:50:03 -0500

    Cambios

--- app/components/Reviews.jsx
+++ app/components/Reviews.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import CardReviews from './CardReviews';
 
 export default function Reviews() {
   return (
     <div className="w-full pt-5 flex justify-between items-center">
-        <h1>Time Line</h1>
+        <CardReviews/>
     </div>
   );
 }
