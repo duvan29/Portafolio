a0c65be37cb82d7557c36f641aeef0b1f3162a73
Duvan Serrano Rojas <duvanserrano21@gmail.com>
2023-10-25 20:41:57 -0500

    Cambios

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Form() {
   return (
     <div className="w-full pt-5 flex justify-between items-center">
-        <h1>Time Line</h1>
+        <h1></h1>
     </div>
   );
 }
